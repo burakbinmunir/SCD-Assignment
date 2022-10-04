6e6364190b1d6592de47192eee7a66829d8e39ff
Burak Bin Munir <burakbinmunir@gmail.com>
2022-10-05 01:14:55 +0500

    Checked cout statement

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
 public class Main {
 
+    public <T> void cout(T a){
+        System.out.println(a);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Initialized the project");
+        Main m = new Main();
+        m.cout("Sallam");
+        m.cout(1);
+        m.cout('s');
+        m.cout(true);
     }
 }
