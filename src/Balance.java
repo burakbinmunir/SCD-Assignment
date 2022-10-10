d42d189b4814d2ee55d909219557727c62d2cfd9
Burak Bin Munir <burakbinmunir@gmail.com>
2022-10-11 00:20:58 +0500

    Done method-1 and method-2 of Question 3

---
+++ src/Balance.java
@@ -0,0 +1,90 @@
+import java.util.Stack;
+
+public class Balance {
+    private Stack<String> stack;
+
+    Balance(){
+        stack = new Stack<String>();
+    }
+
+    public void printStack(){
+        for (String str : stack){
+            System.out.println(str);
+        }
+    }
+
+    public void add(String n){
+        stack.push(n);
+    }
+
+    int doCalculate(int num1, int num2, String operator){
+        if (operator.equals("+")){
+            return num1 + num2;
+        }
+
+        if (operator.equals("-")){
+            return num1 - num2;
+        }
+        if (operator.equals("/")){
+            if (num2 > 0) {
+                return num1 / num2;
+            }
+            return 0;
+        }
+        if (operator.equals("*")){
+            return num1 * num2;
+        }
+        System.out.println("Wrong operator");
+        return -1;
+    }
+
+    public int compute(){
+        if (!stack.isEmpty()) {
+            reverseStack();
+            while (true) {
+                int num1 = Integer.parseInt(stack.pop());
+                String operator = stack.pop();
+                int num2 = Integer.parseInt(stack.pop());
+                int result = doCalculate(num1, num2, operator);
+
+                if (stack.isEmpty()) {
+                    return result;
+                } else {
+                    stack.push(String.valueOf(result));
+                }
+            }
+        }
+        else {
+            System.out.println("Stack is empty");
+            return 0;
+        }
+    }
+
+    void reverseStack(){
+        Stack<String> newStack = new Stack<String>();
+        for(String ele : stack){
+            newStack.push(ele);
+        }
+        stack.clear();
+        for (String ele: newStack ){
+            stack.push(ele);
+        }
+    }
+
+    public static void main(String[] arg){
+        Balance balance = new Balance();
+       balance.add("2");
+        balance.add("+");
+        balance.add("2");
+        balance.add("*");
+        balance.add("2");
+        balance.add("/");
+        balance.add("2");
+        balance.add("*");
+        balance.add("2");
+
+        int sum = balance.compute();
+        System.out.println(sum);
+
+    }
+}
